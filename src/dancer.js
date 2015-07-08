594f5c35c3f11b488b85198047a1928f8e79147e
dfriedenbach <danielfriedenbach@gmail.com>
2015-07-08 16:43:54 -0700

    Added JumpDancer class

--- src/dancer.js
+++ src/dancer.js
@@ -3,9 +3,9 @@ var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  this.setPosition(top, left);
   this.step(timeBetweenSteps);
   // this one sets the position to some random default point within the body
-  this.setPosition(top, left);
 };
 
 Dancer.prototype.step = function(timeBetweenSteps){
@@ -21,6 +21,8 @@ Dancer.prototype.setPosition = function(top, left){
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
+  this.top = top;
+  this.left = left;
   var styleSettings = {
     top: top,
     left: left
---
+++ src/jumpDancer.js
@@ -0,0 +1,17 @@
+var JumpDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+};
+
+JumpDancer.prototype = Object.create(Dancer.prototype);
+JumpDancer.prototype.constructor = JumpDancer;
+JumpDancer.prototype.step = function(timeBetweenSteps){
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this, timeBetweenSteps);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.animate({'top': (this.top - 15)}, 'fast');
+  this.$node.animate({'top': this.top}, 'fast');
+
+};
