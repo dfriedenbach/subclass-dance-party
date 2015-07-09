d137b4d7015273b7d7eee528b5f76f1f50ba172a
dfriedenbach <danielfriedenbach@gmail.com>
2015-07-08 21:07:18 -0700

    Randomize dancer color for new dancers

--- src/dancer.js
+++ src/dancer.js
@@ -7,6 +7,7 @@ var Dancer = function(top, left, timeBetweenSteps){
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   this.setPosition(top, left);
   this.step(timeBetweenSteps);
+  this.setColor(this.colors[Math.floor(Math.random() * this.colors.length)]);
   // this one sets the position to some random default point within the body
 };
 
@@ -31,6 +32,8 @@ Dancer.prototype.setPosition = function(top, left){
   };
   this.$node.css(styleSettings);
 };
+
+Dancer.prototype.colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet'];
 
 Dancer.prototype.setColor = function(color) {
   this.$node.css('border-color', color);
