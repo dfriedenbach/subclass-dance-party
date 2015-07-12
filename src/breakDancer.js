644a7be9f5f5fc3d56c1f2f5e0e0eb50785bef9d
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-07-11 20:24:33 -0700

    Add dance moves to BreakDancer

--- src/breakDancer.js
+++ src/breakDancer.js
@@ -9,7 +9,27 @@ BreakDancer.prototype.constructor = BreakDancer;
 BreakDancer.prototype.step = function(timeBetweenSteps){
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this, timeBetweenSteps);
-  this.$node.animate({'top': (this.top - 35)}, 'fast');
-  this.$node.animate({'top': this.top}, 'fast');
-
+  this.$node.removeClass('front-flip spin-flip rev-spin-flip floor-spin rev-floor-spin head-spin');
+  this.danceMoves[Math.floor(Math.random() * this.danceMoves.length)].call(this);
 };
+
+BreakDancer.prototype.danceMoves = [
+  function() {
+    this.$node.addClass('front-flip');
+  },
+  function() {
+    this.$node.addClass('spin-flip');
+  },
+  function() {
+    this.$node.addClass('rev-spin-flip');
+  },
+  function() {
+    this.$node.addClass('floor-spin');
+  },
+  function() {
+    this.$node.addClass('rev-floor-spin');
+  },
+  function() {
+    this.$node.addClass('head-spin')
+  }
+]
